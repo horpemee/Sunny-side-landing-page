fb8474b858af91b7a2a010ba0502100d2e260905
horpemee <arifoopeyemi@gmail.com>
2023-03-14 19:37:24 +0100

    different images for different screen sizes

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -9,13 +9,13 @@ const Footer = () => {
 
       <ul className="links">
         <li>
-          <a href="#">About</a>
+          <a href="/">About</a>
         </li>
         <li>
-          <a href="#">Services</a>
+          <a href="/">Services</a>
         </li>
         <li>
-          <a href="#">Projects</a>
+          <a href="/">Projects</a>
         </li>
       </ul>
 
