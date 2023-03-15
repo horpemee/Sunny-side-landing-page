87a01e8bf16176d99c39207acbdb992aac523088
horpemee <arifoopeyemi@gmail.com>
2023-03-15 08:07:35 +0100

    removed container div

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -10,44 +10,42 @@ const Navbar = () => {
   // const [showdropDown, setShowDropDown] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
   return (
-    <div className="wrapper">
-      <div className="hero">
-        <div className="main-container">
-          <img src={logo} alt="logo" className="logo" />
-          <ul
-            className={isMobile ? "navlist-mobile" : "navList"}
-            onClick={() => setIsMobile(false)}
-          >
-            <li className="about">
-              <a href="#about">About</a>
-            </li>
-            <li className="service">
-              <a href="#services">Services</a>
-            </li>
-            <li className="project">
-              <a href="#project">Projects</a>
-            </li>
-            <li className="contact">
-              <span className="spans">
-                <a href="#span" className="contact">
-                  Contact
-                </a>
-              </span>
-            </li>
-          </ul>
+    <div className="hero">
+      <div className="main-container">
+        <img src={logo} alt="logo" className="logo" />
+        <ul
+          className={isMobile ? "navlist-mobile" : "navList"}
+          onClick={() => setIsMobile(false)}
+        >
+          <li className="about">
+            <a href="#about">About</a>
+          </li>
+          <li className="service">
+            <a href="#services">Services</a>
+          </li>
+          <li className="project">
+            <a href="#project">Projects</a>
+          </li>
+          <li className="contact">
+            <span className="spans">
+              <a href="#span" className="contact">
+                Contact
+              </a>
+            </span>
+          </li>
+        </ul>
 
-          <div
-            className="mobile-hamburger"
-            onClick={() => setIsMobile(!isMobile)}
-          >
-            {isMobile ? <CloseIcon /> : <img src={hamburger} alt="hamburger" />}
-          </div>
+        <div
+          className="mobile-hamburger"
+          onClick={() => setIsMobile(!isMobile)}
+        >
+          {isMobile ? <CloseIcon /> : <img src={hamburger} alt="hamburger" />}
         </div>
+      </div>
 
-        <div className="heroContent">
-          <h1>We are creatives</h1>
-          <img src={arrow} alt="arrow" className="arrow" />
-        </div>
+      <div className="heroContent">
+        <h1>We are creatives</h1>
+        <img src={arrow} alt="arrow" className="arrow" />
       </div>
     </div>
   );
